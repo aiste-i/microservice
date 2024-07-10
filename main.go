8e8dc3f0874602fdba068897a17108c298a972bf
aiste <aiste.ivanauskaite@mif.stud.vu.lt>
2024-07-10 11:43:17 +0300

    Graceful shutdown added

--- main.go
+++ main.go
@@ -3,14 +3,21 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/aiste-i/mock-microservice/app"
 )
 
 func main() {
 	app := app.New()
-	err := app.Start(context.TODO())
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	err := app.Start(ctx)
 	if err != nil {
 		fmt.Println("failed to start app:", err)
 	}
+
 }
